77807f23b061f1506c227a44c94732c789adef97
Praveen775D <upraveen776@gmail.com>
2026-05-05 09:57:49 +0530

    Initialize React project using Vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,11 @@
+// Vite configuration file for the React client application
+
+// client/vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react()],
 })
